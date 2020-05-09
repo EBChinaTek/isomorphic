10e3032bc33ca978778831808b76f6244dbf178a
lchrennew <lchrennew@126.com>
2020-05-10 06:41:55 +0800

    add the 10th case - should fail

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,3 +6,4 @@ function whatToSay(sequence){
 console.assert(whatToSay(5)==='Buzz', '第五个同学应该说Buzz')
 [1,2,4,7,8,11,13,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`))
 [3,6,9].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`)
+console.assert(whatToSay(10)==='Buzz', '第十个同学应该说Buzz')
