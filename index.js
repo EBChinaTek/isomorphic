b2f8a177d2f2b55c7f4b70df159077b43992fce2
田胜辉 <tianhuihebei@foxmail.com>
2020-05-09 08:10:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ function isomorphic(s, t) {
   if (s.length !== t.length) { // 如果两个字符串长度不相等，则两个字符串不是同构的
     return false
   }
-  let obj = {} // 用对象来存储字符之间的映射关系
-  for (let i = 0; i < s.length; i++) {
-    // 判断字符串s中的字符是否存在于obj中
-    if(typeof obj[s[i]] !== 'undefined'){
-      // 如果字符串t中对应的字符与obj中的映射字符不同，则不是同构
-      if(t[i] !== obj[s[i]]){
+  
+  for(let i = 0; i < s.length; i ++){
+    // 判断两个字符串中相同位置对应的字符在两个字符串中首次出现的位置是否相同
+    // 如果相同，则是两个字符串中的字符是一一对应关系
+    // 如果不同，则两个字符串不是同构
+    if(s.indexOf(s[i]) !== t.indexOf(t[i])){
         return false
-      }
-    }else{
-      // 如果不存在，将字符串s中的当前字符与t中对应位置的字符的映射关系保存到obj中
-      obj[s[i]] = t[i]
     }
   }
   return true
 }
 
-let s = 'egg', t = 'add'
+let s = 'ab', t = 'aa'
 console.log(isomorphic(s, t))
