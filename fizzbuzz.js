dade2dadcba6284c13f7d9aefc38cefe0ff33963
lchrennew <lchrennew@126.com>
2020-05-10 06:18:00 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,4 +4,4 @@ function whatToSay(sequence){
 
 console.assert(whatToSay(1)==='1', '第一个同学应该说1')
 console.assert(whatToSay(2)==='2', '第二个同学应该说2')
-
+console.assert(whatToSay(3)==='Fizz', '第三个同学应该说Fizz')
