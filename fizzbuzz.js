57db0b7dd0e5ca44791aa98f2daf76ff9dab4733
lchrennew <lchrennew@126.com>
2020-05-10 07:35:10 +0800

    add a case for 13th

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,10 +5,12 @@ function whatToSay(sequence){
   return `${sequence}`
 }
                    
-[1,2,4,7,8,11,13,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`));
+[1,2,4,7,8,11,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`));
 [3,6,9,12, 96, 99].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`));
 [5,10, 85, 95].forEach(sequence=>console.assert(whatToSay(sequence)==='Buzz', `第${sequence}个同学应该说Buzz`));
 [15, 30, 45, 60, 75, 90].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`));
+
+console.assert(whatToSay(13)==='Buzz', `第${sequence}个同学应该说Buzz`)
 
 function printFullList(sequenceFrom, sequenceTo){
   for(let sequence = sequenceFrom; sequence <= sequenceTo; sequence++){
