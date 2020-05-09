b7040d1ed3e5616c4a68433e965da69a331743df
lchrennew <lchrennew@126.com>
2020-05-10 07:41:35 +0800

    refactor test cases

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,12 +7,9 @@ function whatToSay(sequence){
 }
                    
 [1,2,4,7,8,11,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`));
-[3,6,9,12, 96, 99].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`));
+[3,6,9,12,13,23,30,31,33,83, 96, 99].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`));
 [5,10, 85, 95].forEach(sequence=>console.assert(whatToSay(sequence)==='Buzz', `第${sequence}个同学应该说Buzz`));
-[15, 30, 45, 60, 75, 90].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`));
-
-console.assert(whatToSay(13)==='Fizz', `第${sequence}个同学应该说Fizz`);
-console.assert(whatToSay(23)==='Fizz', `第${sequence}个同学应该说Fizz`)
+[15, 45, 60, 75, 90].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`));
 
 function printFullList(sequenceFrom, sequenceTo){
   for(let sequence = sequenceFrom; sequence <= sequenceTo; sequence++){
