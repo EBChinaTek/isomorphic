6214940b6036d27df19699d9281c20cfbd4588c2
lchrennew <lchrennew@126.com>
2020-05-10 06:54:47 +0800

    refactor test cases

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,6 +10,5 @@ function whatToSay(sequence){
 [1,2,4,7,8,11,13,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`))
 [3,6,9,12].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`)
 [5,10].forEach(sequence=>console.assert(whatToSay(sequence)==='Buzz', `第${sequence}个同学应该说Buzz`)
-console.assert(whatToSay(15)==='FizzBuzz', '第15个同学应该说FizzBuzz')
-console.assert(whatToSay(30)==='FizzBuzz', '第30个同学应该说FizzBuzz')
+[15, 30].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`)
 
