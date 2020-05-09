3c11f7d0bc7c81164d47f6257ec1d27e4568a1a2
lchrennew <lchrennew@126.com>
2020-05-10 06:55:40 +0800

    add more test cases - should pass

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,5 +10,5 @@ function whatToSay(sequence){
 [1,2,4,7,8,11,13,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`))
 [3,6,9,12].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`)
 [5,10].forEach(sequence=>console.assert(whatToSay(sequence)==='Buzz', `第${sequence}个同学应该说Buzz`)
-[15, 30].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`)
+[15, 30, 45, 60, 75].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`)
 
