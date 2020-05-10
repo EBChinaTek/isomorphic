7cff3916a771f5085e15082e98205f7859a848f5
lchrennew <lchrennew@126.com>
2020-05-10 08:57:01 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,10 @@
-function divisible(dividend, divisor){
-  return dividend%divisor===0
-}
+const divisible = (dividend, divisor)=>dividend%divisor===0,
+      contains  = (number, digitValue)=>`${number}`.indexOf(`${digitValue}`)>=0
 
-
-function contains(number, digitValue){
-  return `${number}`.indexOf(`${digitValue}`)>=0
-}
-
-console.assert(contains(51, 5))
-
-const devisibleByThree = sequence => divisible(sequence, 3)
-const containsThree = sequence=> contains(sequence, 3)
-const divisibleByFive = sequence=> divisible(sequence, 5)
-const containsFive = sequence=> contains(sequence, 5)
+const devisibleByThree = sequence => divisible(sequence, 3),
+      containsThree = sequence=> contains(sequence, 3),
+      divisibleByFive = sequence=> divisible(sequence, 5),
+      containsFive = sequence=> contains(sequence, 5);
 
 const rules = {
   'Fizz': [devisibleByThree,containsThree],
