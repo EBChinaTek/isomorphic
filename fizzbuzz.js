3a862645b0bd88473b098ee9912db0355ec300be
lchrennew <lchrennew@126.com>
2020-05-10 08:17:19 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,16 +2,21 @@ function divisible(dividend, divisor){
   return dividend%divisor===0
 }
 
+
 function contains(number, digitValue){
   return `${number}`.indexOf(`${digitValue}`)>=0
 }
+
+
+console.assert(contains(51, 5))
 
 function whatToSay(sequence){
   const devisibleByThree = divisible(sequence, 3)
   const divisibleByFive = divisible(sequence, 5)
   const containsThree = contains(sequence, 3)
   const containsFive = contains(sequence, 5)
-  if((devisibleByThree && divisibleByFive) || (containsThree && containsFive)) return ['Fizz','Buzz'].join('')
+  
+  if((devisibleByThree && (divisibleByFive || containsFive)) || (containsThree && (divisibleByFive || containsFive))) return ['Fizz','Buzz'].join('')
   if(devisibleByThree || containsThree) return ['Fizz'].join('')
   if(divisibleByFive || containsFive) return ['Buzz'].join('')
   
@@ -19,9 +24,9 @@ function whatToSay(sequence){
 }
                    
 [1,2,4,7,8,11,14,16].forEach(sequence=>console.assert(whatToSay(sequence)===`${sequence}`, `第${sequence}个同学应该说${sequence}`));
-[3,6,9,12,13,23,30,31,33,83, 96, 99].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`));
-[5,10,51,52, 85, 95].forEach(sequence=>console.assert(whatToSay(sequence)==='Buzz', `第${sequence}个同学应该说Buzz`));
-[15, 35, 45, 53, 60, 75, 90].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`));
+[3,6,9,12,13,23, 31,33,83, 96, 99].forEach(sequence=>console.assert(whatToSay(sequence)==='Fizz', `第${sequence}个同学应该说Fizz`));
+[5,10,52, 85, 95].forEach(sequence=>console.assert(whatToSay(sequence)==='Buzz', `第${sequence}个同学应该说Buzz`));
+[15, 30, 35, 45,51, 53, 60, 75, 90].forEach(sequence=>console.assert(whatToSay(sequence)==='FizzBuzz', `第${sequence}个同学应该说FizzBuzz`));
 
 
 function printFullList(sequenceFrom, sequenceTo){
